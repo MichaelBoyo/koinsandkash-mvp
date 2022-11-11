39c2bf7a3b4d91f629b49c845332ef4545a9f7f4
MichaelBoyo <boyomichaelbidemi@gmail.com>
2022-11-11 15:03:37 +0100

    signup comp

--- src/pages/AppBar.js
+++ src/pages/AppBar.js
@@ -3,9 +3,9 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
 
 export default function ButtonAppBar() {
   const navigate = useNavigate();
@@ -23,7 +23,7 @@ export default function ButtonAppBar() {
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               <img
                 style={{
-                width: "100px"
+                  width: "100px",
                 }}
                 src="https://res.cloudinary.com/dfsn2ob9s/image/upload/v1668165833/K_K_logo_6_cg6ira.png"
                 alt="KoinsAndKash"
@@ -35,13 +35,18 @@ export default function ButtonAppBar() {
             component="div"
             sx={{ flexGrow: 1 }}
           ></Typography>
-          <Button onClick={()=> navigate(`/${window.location.pathname === "/" ? "login" : ""}`)} sx={{
-                backgroundColor: "white",
-                font: "bold",
-                color :"#1976d2"
-          }} variant="contained">
+          <Button
+            onClick={() =>
+              navigate(`${window.location.pathname === "/" ? "/login" : "/"}`)
+            }
+            style={{
+              backgroundColor: "white",
+              color: "#1976d2",
+            }}
+            variant="contained"
+          >
             {window.location.pathname === "/" ? "Login" : "Register"}
-         
+           
           </Button>
         </Toolbar>
       </AppBar>
